67acdb8841698f1501d24efb4b3c9903015d115e
sarajouma <144836947+sarajouma@users.noreply.github.com>
2023-10-06 21:51:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity ="roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+function mondayWork(activity ="go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair = "*") {
+if(flair == "*"){
+   
+    return   function () { return 'You are '+flair+'a hard worker'+flair+'!';}
+    }
+   else if(flair == "||"){
+    return   function () { return 'You are '+flair+'a dedicated programmer'+flair+'!';}
+     
+   }
+}
